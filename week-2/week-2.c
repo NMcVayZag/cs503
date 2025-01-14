928a028787599ba1f8e38a96e5398944bee7ea7f
NMcVayZag <Nickwmcvay15@gmail.com>
2025-01-13 17:51:44 -0800

    change repo structure

---
+++ week-2/week-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello, World!\n");
+    return 0;
+}
+//Concept: main() is the entry point of the program
+
+// int main(int argc, char *argv[]){
+//     if (argc>=2) {
+//         return argc;
+//     }
+//     return 0;
+// }
+
+
